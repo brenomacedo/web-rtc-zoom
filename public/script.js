b97a03f085c5c4b2e9c0404bfb8cbc4dca0a306d
brenosekiro <b88660501@gmail.com>
2020-11-18 22:52:37 -0300

    webRTC

--- public/script.js
+++ public/script.js
@@ -1,7 +1,19 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+}).catch(err => {
+    alert('erro ai')
 })
 
 myPeer.on('open', id => {
@@ -11,3 +23,12 @@ socket.emit('join-room', ROOM_ID, 10)
 socket.on('user-connected', userId => {
     console.log('User conneted: ' + userId)
 })
+
+function addVideoStream (video, stream) {
+    console.log(stream)
+    video.srcObject = stream
+    video.addEventListener('loadedMetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
